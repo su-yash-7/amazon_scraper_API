3a88072cfba76016ebe32a883ca5366c084dcaf1
su-yash-7 <suyashpradhan7@gmail.com>
2021-08-02 11:38:35 +0530

    Get search results endpoint

--- index.js
+++ index.js
@@ -54,4 +54,19 @@ app.get('/products/:productId/offers', async (req, res) => {
     res.json(error);
   }
 });
+// GET search results
+app.get('/search/:searchQuery', async (req, res) => {
+  const { searchQuery } = req.params;
+
+  try {
+    const response = await request(
+      `${baseUrl}&url=https://www.amazon.com/s?k=${searchQuery}`
+    );
+
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+
 app.listen(PORT, () => console.log(`Server Running on port  ${PORT}`));
